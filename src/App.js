2ff2c31e45090f51d5937b450915ff91e027acd1
revivesanjeev <revivesanjeev324@gmail.com>
2024-06-30 16:03:28 +0530

    writing the code using async await

--- src/App.js
+++ src/App.js
@@ -6,12 +6,11 @@ import './App.css';
 function App() {
   const [Movies,setMovies]=useState([]);
 
-  function fetchMoviesHandler(){
-    fetch ('https://swapi.dev/api/films/')
-    .then(response=>{
-      return response.json();
-    })
-    .then (data=> {
+  async function fetchMoviesHandler(){
+     const response= await fetch ('https://swapi.dev/api/films/')
+    const data=await  response.json();
+    
+    
        const transformedMovies=data.results.map(movieData=>{
         return {
           id:movieData.episode_id,
@@ -21,7 +20,7 @@ function App() {
         };
        });
       setMovies(transformedMovies);
-    });}
+   }
   return (
     <React.Fragment>
       <section>
